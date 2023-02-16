0bbf6b0ae2ed27260b2e250270d044ff16f39458
Luthfii1 <luthfi.misbachul@ui.ac.id>
2023-02-16 14:39:02 +0700

    Convert to CSV until 49 people

--- linkedin.py
+++ linkedin.py
@@ -11,52 +11,34 @@ import re
 import xlrd
 import numpy as np
 import math
+import csv
 
 # using chrome driver 
-driver = webdriver.Chrome(executable_path=r"C:\Users\luthf\Downloads\chromedriver\chromedriver.exe")
+driver = webdriver.Chrome(executable_path=r"C:\Users\luthf\Downloads\chromedriver_win32\chromedriver.exe")
 
 # path of the file
 path = r'C:\Users\luthf\Desktop\Coding\Python\Web scrapping for data alumni tsa ui\Database Scholars 2018 - 2023 - master.csv'
 df = pd.read_csv(path)
 
-# Data that we need:
-# From CSV file:
-# 1. LinkedIn
-# 2. Tahun TF
-# 3. Fakultas
-# 4. Jurusan
-# 5. Tanggal Lahir
-# From Scrapping:
-# 1. Nama
-# 2. desc
-# 3. recent work or experience
-# 4. job_detail
-# 5. Major (gausah kayanya)
-# 6. Year Graduate 
-# 7. Year Tanoto (gausah kayanya)
-# 8. isGraduate 
-
 # get the data from the csv file
-# name = list(df['Nama Lengkap'])
+name = list(df['Nama Lengkap'])
+header = ['Nama Lengkap', 'Tanggal Lahir', 'desc', 'Fakultas', 'Jurusan', 'Tahun TF', 'Year Graduate', 'isGraduate',  'recent work or experience', 'job_detail', 'Whatsapp', 'ID Line', 'LinkedIn']
 linkedin = list(df['LinkedIn'])
 year_tanoto_csv = list(df['Tahun TF'])
 fakultas = list(df['Fakultas'])
 jurusan = list(df['Jurusan'])
 born_date = list(df['Tanggal Lahir'])
-whatsapp = list(df['WhatsApp'])
+whatsapp = list(df['Whatsapp'])
 id_line = list(df['ID Line'])
 
-
+    
 # login
 url_login = 'https://www.linkedin.com/'
 url_feed = 'https://www.linkedin.com/feed/'
 
-# data scrapping
-# data = ["https://www.linkedin.com/in/gracellajovita/", "https://www.linkedin.com/in/salma-dewi-taufiqoh-436548183/"]
-
 # username and password for login
-username = 'luthfimisbachulmunir@gmail.com'
-password = 'linkedin123321'
+username = 'username'
+password = 'password'
 
 # get the url
 driver.get(url_login)
@@ -77,138 +59,123 @@ if driver.find_element(By.ID, 'session_key'):
 else: # if the element is not exist
     print("Already login")
 
-time.sleep(30)
+time.sleep(60)
 
 driver.get(url_feed)
 print("Success go to feed page")
 count = 0
-# remove the null, nan and the space value using if else
-for i in linkedin:
-    print("\n" +  str(count + 2))
-    count += 1
-    check = str(i)
-    err = count + 3
-    tanggal_lahir = str(born_date[count-1])
-    major_person = str(jurusan[count-1])
-    faculty_person = str(fakultas[count-1])
-    year_tanoto_person = str(year_tanoto_csv[count-1])
-    whatsapp_person = str(whatsapp[count-1])
-    id_line_person = str(id_line[count-1])
-    year_graduate_person = int(year_tanoto_person) + 3
-    year_graduate = str(year_graduate_person)
-    # if the data linkedin is null, nan or space skip the get data from the linkedin
-    if check == 'nan' or check == 'null' or check == ' ':
-        print("All the data is null\n")
-        
-        # print('Name : ' + name)
-        # print('linkedin_url : ' + linkedin_url)
-        # print('Tanggal lahir : ' + tanggal_lahir)
-        # print('desc : ' + desc)
-        # print('recent work or experience : ' + company_name)
-        # print('job_detail : ' + job_detail)
-        # print('Faculty : ' + faculty)
-        # print('Major : ' +  major)
-        # print('Tahun lulus : ' + str(year_graduate))
-        # print('Angkatan tanoto : ' +  str(year_tanoto))
-        # print('isGraduate : ' + isGraduate)
-        continue
-    # if the data linkedin is not null, nan or space get the data from the linkedin
-    else:
-        driver.get(i)
-        response = requests.get(i)
-        sel = Selector(text=driver.page_source)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-        time.sleep(3)
+# Create a new csv file
+with open('Database Scholars New.csv', 'a', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+    
+    # write the header
+    # writer.writerow(header)
+    # remove the null, nan and the space value using if else
+    for i in linkedin[48:]:
+        print("\n" +  str(count + 2))
+        count += 1
+        check = str(i)
+        err = count + 3
+        tanggal_lahir = str(born_date[count-1])
+        major_person = str(jurusan[count-1])
+        faculty_person = str(fakultas[count-1])
+        year_tanoto_person = str(year_tanoto_csv[count-1])
+        whatsapp_person = str(whatsapp[count-1])
+        id_line_person = str(id_line[count-1])
+        year_graduate_person = int(year_tanoto_person) + 3
+        year_graduate = str(year_graduate_person)
+        name_person = name[linkedin.index(i)]
+        isGraduate = 'Graduate' if year_graduate_person <= 2023 else 'Not Graduate'
+        if whatsapp_person == 'nan':
+            whatsapp_person = ' '
+        if id_line_person == 'nan':
+            id_line_person = ' '
+        # if the data linkedin is null, nan or space skip the get data from the linkedin
+        if check == 'nan' or check == 'null' or check == ' ':
+            print("All the data is null\n")
+            desc = 'null'
+            company_name = 'null'
+            company_recent = 'null'
+            job_detail = 'null'
+            isGraduate = 'null'
+            linkedin_url = 'null'
+            data = [name_person, tanggal_lahir, desc, faculty_person, major_person, year_tanoto_person, year_graduate, isGraduate, company_recent, job_detail, whatsapp_person, id_line_person, linkedin_url]
+            writer.writerow(data)
+            continue
+        # if the data linkedin is not null, nan or space get the data from the linkedin
+        else:
+            driver.get(i)
+            response = requests.get(i)
+            sel = Selector(text=driver.page_source)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-        name = driver.find_element(By.CSS_SELECTOR, 'h1.text-heading-xlarge.inline.t-24.v-align-middle.break-words').text
-        # tanggal_lahir = 'null'
-        # faculty = 'null'
+            time.sleep(3)
 
-        desc = driver.find_element(By.CSS_SELECTOR, 'div.text-body-medium.break-words').text
+            # name = driver.find_element(By.CSS_SELECTOR, 'h1.text-heading-xlarge.inline.t-24.v-align-middle.break-words').text
 
-        # get the linkedin url
-        linkedin_url = driver.current_url
+            desc = driver.find_element(By.CSS_SELECTOR, 'div.text-body-medium.break-words').text
 
-        # get the company name
-        try:
-            company_recent = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[1]/div[2]/div[2]/ul/li[1]/button/span').text
-            company_name, sep, tail = company_recent.partition('\n')
-        except:
-            company_name = "Null"
+            # get the linkedin url
+            linkedin_url = driver.current_url
 
-        # get the job detail
-        try:
-            temp = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[5]/div[3]/ul/li[1]/div/div[2]/div[2]/ul/li[1]/div/div[2]/div/a/div').text
-        except:
-            temp = "Not yet work"
-
-        job_detail, sep, tail = temp.partition('\n')
-
-        # get the year graduate
-        try:
-            school_recent = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[6]/div[3]/ul/li[1]/div/div[2]/div[1]').text
-        except:
+            # get the company name
             try:
-                school_recent = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[5]/div[3]/ul/li[1]/div/div[2]/div[1]').text
+                company_recent = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[1]/div[2]/div[2]/ul/li[1]/button/span').text
+                company_name, sep, tail = company_recent.partition('\n')
+            except:
+                company_name = "Null"
+
+            # get the job detail
+            try:
+                temp = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[5]/div[3]/ul/li[1]/div/div[2]/div[2]/ul/li[1]/div/div[2]/div/a/div').text
+            except:
+                temp = "Not yet work"
+
+            job_detail, sep, tail = temp.partition('\n')
+
+            # get the year graduate
+            try:
+                school_recent = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[6]/div[3]/ul/li[1]/div/div[2]/div[1]').text
             except:
                 try:
-                    school_recent = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[4]/div[3]/ul/li[1]/div/div[2]/div[1]').text
+                    school_recent = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[5]/div[3]/ul/li[1]/div/div[2]/div[1]').text
                 except:
                     try:
-                        school_recent = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[3]/div[3]/ul/li[1]/div/div[2]/div[1]').text
+                        school_recent = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[4]/div[3]/ul/li[1]/div/div[2]/div[1]').text
                     except:
                         try:
-                            school_recent = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[2]/div[3]/ul/li[1]/div/div[2]/div[1]').text
+                            school_recent = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[3]/div[3]/ul/li[1]/div/div[2]/div[1]').text
                         except:
-                            school_recent = "Null"
-            
+                            try:
+                                school_recent = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[2]/div[3]/ul/li[1]/div/div[2]/div[1]').text
+                            except:
+                                school_recent = "Null"
+                
 
-        head, sep, tail = school_recent.partition('\nUniversity of Indonesia\n')
-        head, sep, tail = tail.partition('\n')
-        major, sep, tail = tail.partition('\n')
+            head, sep, tail = school_recent.partition('\nUniversity of Indonesia\n')
+            head, sep, tail = tail.partition('\n')
+            major, sep, tail = tail.partition('\n')
 
-        if tail == '': 
-            # year_tanoto = "Null"
-            year_graduate = "Null"
-            isGraduate = "Null"
+        if err == 57:
+            continue
         else:
-            head, sep, duration = tail.partition('\n')
-            duration, sep, tail = tail.partition('\n')
-            year_tanoto, sep, graduate = tail.partition(' - ')
-            # keep only nmber from string year_tanoto
-            year_tanoto = re.sub("[^0-9]", "", year_tanoto)
-            year_tanoto = int(year_tanoto)
-            year_tanoto += 1
+            rowlist = [name_person, tanggal_lahir, desc, faculty_person, major_person, year_tanoto_person, year_graduate_person, isGraduate, company_name, job_detail, whatsapp_person, id_line_person, linkedin_url]
+            writer.writerow(rowlist)
 
-            if graduate == '':
-                graduate = "Null"
-            else:
-                # keep only nmber from string graduate
-                year_graduate = re.sub("[^0-9]", "", graduate)
-                year_graduate = int(year_graduate)
-                # check if the graduate or not
-                if int(year_graduate) < 2023:
-                    isGraduate = 'Graduated'
-                else:
-                    isGraduate = 'Not Yet'
-
-    if err == 57:
-        continue
-    else:
-        print('Name : ' + name)
-        print('linkedin_url : ' + linkedin_url)
-        print('Tanggal lahir : ' + tanggal_lahir)
-        print('desc : ' + desc)
-        print('recent work or experience : ' + company_name)
-        print('job_detail : ' + job_detail)
-        print('Faculty : ' + faculty_person)
-        print('Major : ' +  major_person)
-        print('Tahun lulus : ' + str(year_graduate_person))
-        print('Angkatan tanoto : ' +  year_tanoto_person)
-        print('isGraduate : ' + isGraduate)
-        print('whatsapp : ' + whatsapp_person)
-        print('id_line : ' + id_line_person)
+            print('Name : ' + name_person)
+            print('linkedin_url : ' + linkedin_url)
+            print('Tanggal lahir : ' + tanggal_lahir)
+            print('desc : ' + desc)
+            print('recent work or experience : ' + company_name)
+            print('job_detail : ' + job_detail)
+            print('Faculty : ' + faculty_person)
+            print('Major : ' +  major_person)
+            print('Tahun lulus : ' + str(year_graduate_person))
+            print('Angkatan tanoto : ' +  year_tanoto_person)
+            print('isGraduate : ' + isGraduate)
+            print('whatsapp : ' + whatsapp_person)
+            print('id_line : ' + id_line_person)
 
 # remove the null, nan and the space value using list comprehension
 # linkedin = [x for x in linkedin if str(x) != 'nan']
---
+++ testing.py
@@ -0,0 +1,21 @@
+import csv
+import pandas as pd
+
+header = ['Name', 'Title', 'Company', 'Location', 'Education', 'Major', 'Faculty', 'Year Tanoto', 'Year Graduate', 'Whatsapp', 'ID Line', 'Linkedin']
+data = ['Luthfi', 'lorem i', 'lorem i', 'lorem', 'lorem', 'lorem', 'lorem', 'lorem', 'lorem', 'lorem', 'lorem', 'lorem']
+
+with open('Database Scholars New.csv', 'w', encoding='UTF8') as f:
+    reader = csv.reader(f)
+    writer = csv.writer(f)
+    
+    # print the header to csv
+    writer.writerow(header)
+    
+# path of the file
+path = r'C:\Users\luthf\Desktop\Coding\Python\Web scrapping for data alumni tsa ui\Database Scholars 2018 - 2023 - master.csv'
+df = pd.read_csv(path)
+
+name = list(df['Nama Lengkap'])
+linkedin = list(df['LinkedIn'])
+for i in name:
+    print(i)
